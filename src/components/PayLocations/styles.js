e26e7af3307d43c8cd44bffea6d77081e39ec92b
Leonardo Lima <leonardodelima96@hotmail.com.br>
2020-06-30 17:36:29 -0300

    Implementação da tela de locais na região

--- src/components/PayLocations/styles.js
+++ src/components/PayLocations/styles.js
@@ -1,7 +1,45 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.ScrollView``;
+export const Container = styled.View`
+    flex: 1;
+    background: #1e222b;
+`;
 
-export const Title = styled.Text`
-
+export const Locations = styled.ScrollView`
+    padding: 16px 16px;
 `
+
+export const MarketPlace = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+export const Img = styled.Image`
+    width: 60px;
+    height: 60px;
+    border-radius: 150px;
+`
+
+export const InfoLocation = styled.View`
+    flex-direction: column;
+    margin: 0 16px;
+    width: 70%;
+`
+
+export const TitleLocation = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+export const Label = styled.Text`
+    color: #fff;
+    font-weight: bold;
+    margin-right: 5px;
+`
+
+export const DescriptionLocation = styled.Text`
+    color: #fff;
+    font-size: 12px;
+`
+
+export const MenuLocation = styled.View``
